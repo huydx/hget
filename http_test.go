756bfcbed404b8909f67c0a238121c91c160885b
Abouzar Parvan <abzcoding@users.noreply.github.com>
2020-08-19 15:59:33 +0430

    Update http_test.go

--- http_test.go
+++ http_test.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"testing"
 	"os/user"
 	"path/filepath"
+	"testing"
 )
 
 func TestPartCalculate(t *testing.T) {
@@ -13,15 +13,22 @@ func TestPartCalculate(t *testing.T) {
 	if len(parts) != 10 {
 		t.Fatalf("parts length should be 10")
 	}
+
 	if parts[0].Url != "http://foo.bar/file" {
 		t.Fatalf("part url was wrong")
 	}
+
 	usr, _ := user.Current()
-	dir := filepath.Join(usr.HomeDir, dataFolder, "file/file.part0")
-	if parts[0].Path != dir {
+	dir := filepath.Join(usr.HomeDir, dataFolder, "file/file.part000001")
+	if parts[1].Path != dir {
 		t.Fatalf("part path was wrong")
 	}
+
 	if parts[0].RangeFrom != 0 && parts[0].RangeTo != 10 {
 		t.Fatalf("part range was wrong")
+	}
+
+	if parts[1].Index != 1 {
+		t.Fatal("part index was wrong")
 	}
 }
